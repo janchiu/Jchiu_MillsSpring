cf61435bfa37410869572d78171f2b4ed74a13d0
janchiu <17chiuj39@millshighsch.org>
2017-03-03 08:44:20 -0800

    fixed main

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -8,9 +8,20 @@ import java.util.Scanner;
 public class TextExcel
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
+		Scanner userInput = new Scanner(System.in);
+		Spreadsheet jdog = new Spreadsheet(); 
+		String[][] arr1 = new String[20][12];
+		for ( int i = 0; i< arr1.length; i++) {
+			for( int j = 0; j < arr1[i].length; j++){
+			arr1[i][j] = userInput.nextLine(); 
+			}
+		}
+		if(userInput = "quit"){
+			
+		}
 	    // Add your command loop here
-		emptyCell[][] empty = new emptyCell();
+		userInput.close();
 	}
 }
