789f28f87a8eda761133d3cf71ba531f16af9b3d
janchiu <17chiuj39@millshighsch.org>
2017-03-15 22:37:59 -0700

    changes

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -37,7 +37,7 @@ public class Spreadsheet implements Grid
 	}
 	
 
-
+/*
 	public String getGridText()
 	{
 		String[] letters = {"A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
@@ -61,5 +61,51 @@ public class Spreadsheet implements Grid
 		}
 		return "";
 		//abbreviatedCellText();
+	}
+}
+*/
+	public  String getGridText()
+	{
+		String alphabet= "ABCDEFGHIJKL";
+		String firstline;
+		String grid="";
+		//12
+		//first row 
+		firstline= (print3spaces());
+		for (int i=0; i<=11;i++){
+			firstline+=("|" + alphabet.charAt(i)+ print9spaces());
+		}
+		firstline+= ("\n");
+		for (int i=1; i<=20; i++){
+			if (i<10){
+				grid+= (i + print2spaces()+ "\n");
+				for (int j=1; j<=12;i++){
+					grid+=("|" + print10spaces());
+				}
+			}
+			if (i>=10){
+				grid+=(i + " " + "\n");
+				for (int k=1; k<=12;i++){
+					grid+=("|" + print10spaces());
+			}
+			
+		}
+	}
+		return firstline + grid;
+}
+	
+	public String print9spaces(){
+		return ("         ");
+	}
+
+	public String print3spaces(){
+		return ("   ");
+	}
+	public String print2spaces(){
+		return("  ");
+		
+	}
+	public String print10spaces(){
+		return("          ");
 	}
 }
