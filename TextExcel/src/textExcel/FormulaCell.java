4243da0e21fdd422d575a7756e526029e762355e
janchiu <17chiuj39@millshighsch.org>
2017-05-05 08:52:12 -0700

    changes

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -5,10 +5,16 @@ public class FormulaCell extends RealCell{
 	
 	public FormulaCell(String value, Spreadsheet sheet){
 		super(value);
-		original = sheet;
-		String cellEnter = value.substring(0, 2);
-		sheet = // idk 
-		String formula = value.substring(value.indexOf("("), value.indexOf(")"));
+		//original = sheet;
+		
+		String cellEnter = value.substring(0, 3);
+		if(cellEnter.endsWith(" ")) {
+			cellEnter = cellEnter.substring(0, 2);
+		}
+		//sheet = // idk 
+		String formula = value.substring(value.indexOf("(") +1, value.indexOf(")"));
+		
+		// need to be 
 	}
 	
 	
